1b9bf75875e96ec9c90b2ab97bf811612d979ef2
samo <95826838+jirane@users.noreply.github.com>
2023-02-12 13:16:05 +0100

    add works page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Routes,Route} from 'react-router-dom';
 import Layout from './components/Layout/index'
 import Home from './components/Home/index.js'
 import About from './components/About/index'
-import works from './components/works/index';
+import Works from './components/works/index';
 import Contact from './components/Contact/index'
 import Skills from './components/Skills/index'
 import NotFound from './components/NotFound/index.js'
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<Layout />} >
             <Route index element={<Home />} />
             <Route path='/about' element = {<About />} />
-            <Route path='/works' element = {<works />} />
+            <Route path='/works' element = {<Works />} />
             <Route path='/skills' element={<Skills />} />
             <Route path='/contact' element={<Contact/>} />
             <Route path='*' element={<NotFound />} />
